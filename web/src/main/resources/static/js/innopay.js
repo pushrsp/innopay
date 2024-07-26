b24eef4c53946d6092b98a6e737b8b86a68939a4
pushrsp <pushrsp@gmail.com>
2024-07-26 18:01:28 +0900

    WIP: goPay

--- web/src/main/resources/static/js/innopay.js
+++ web/src/main/resources/static/js/innopay.js
@@ -1,6 +1,33 @@
+const FORM_NAME = "InnopayForm"
+
+function setPadding() {
+}
+
+function createForm(name, method, action, target) {
+    let form = $(`form[name=${name}]`)
+    if (form.length === 0) {
+        form = document.createElement("form")
+    }
+
+    form.name = name
+    form.method = method
+    form.target = target
+
+    document.body.appendChild(form)
+    return form
+}
+
+function setEdiDate(form) {
+    const date = new Date()
+    const h = date.getHours()
+    const m = date.getMinutes()
+    const s = date.getSeconds()
+}
+
 const innopay = {
     goPay: (data) => {
-        console.log("goPay: ", data)
+        const form = createForm(FORM_NAME, "", "", "")
+        setEdiDate(form)
     },
     close: () => {
     }
